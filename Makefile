a718090ad635dcf57b05d0228b614bb91a2eb642
wengdahl <wfengdahl@wpi.edu>
2021-11-14 18:12:46 -0500

    basic code to print once for each packet

---
+++ Makefile
@@ -0,0 +1,19 @@
+SRC_PATH= src/*
+
+CCPP=g++
+CPLUS_INCLUDE_PATH = includes/
+
+all:
+	make init-bin
+	$(CCPP) -o2 -I $(CPLUS_INCLUDE_PATH) $(SRC_PATH) -lpcap -o bin/wireview.out
+
+init-bin:
+	mkdir -p bin
+	mkdir -p bin/Debug
+	
+all-debug:
+	make init-bin
+	$(CCPP) -g -DDEBUG -I $(CPLUS_INCLUDE_PATH) $(SRC_PATH) -o bin/Debug/wireview.out
+
+clean:
+	rm -f -r bin/
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pcap.h>
+
+
+//Callback - Process packet
+void got_packet(u_char *empty, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    printf("Parsing packet\n");
+}
+
+int main(int argc, char *argv[])
+{
+        char *fname = argv[1];
+        char *errbuf;
+        pcap_t *handle;
+
+        //Open offline, usinf argv as name of file to open
+	    handle = pcap_open_offline(fname, errbuf);
+        if (handle == NULL) {
+            fprintf(stderr, "Couldn't open pcap file %s: %s\n", fname, errbuf);
+            return(2);
+        }
+
+        //Check data is from ethernet
+        int datalink = pcap_datalink(handle);
+        if(datalink != 1){
+            fprintf(stderr, "pcap not ethernet, return: %i\n", datalink);
+            return(2);
+        }
+
+        int n = pcap_loop(handle, 0, got_packet, NULL);
+
+        printf("Parsing complete: %i\n",n);
+        return(0);
+}
